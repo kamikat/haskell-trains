5c84c3384e4068b8d26897e5eb13dcfbb8609b14
kamikat <kamikat@banana.moe>
2017-05-28 14:47:15 +0800

    LastDigit: lazier than lazier

--- src/LastDigit.hs
+++ src/LastDigit.hs
@@ -3,7 +3,7 @@
 module LastDigit where
 
 lastDigit :: [Integer] -> Integer
-lastDigit = flip mod 10 . lastDigit'
+lastDigit xs = flip mod 10 . lastDigit' $ xs
 
 -- digitLookup = [ [ let p = x ^ k
 --                       m = p `mod` 20
@@ -22,10 +22,20 @@ digitLookup = [
   [20,20,20,20], [21,31,21,31], [36,32,24,28], [21,33,29,37], [36,24,36,24], [25,35,25,35],
                  [36,36,36,36], [21,37,29,33], [36,28,24,32], [21,39,21,39]]
 
+digitLookup' = [
+  [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,3,1,3], [0,0,0,0], [1,1,1,1],
+             [0,0,0,0], [1,3,1,3], [0,0,0,0], [1,1,1,1],
+  [0,0,0,0], [1,3,1,3], [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,3,1,3],
+             [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,3,1,3],
+  [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,3,1,3], [0,0,0,0], [1,1,1,1],
+             [0,0,0,0], [1,3,1,3], [0,0,0,0], [1,1,1,1],
+  [0,0,0,0], [1,3,1,3], [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,3,1,3],
+             [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,3,1,3]]
+
 lastDigit' []     = 1
 lastDigit' (x:xs) = case xs of (1:_)   -> lookup 1
                                (0:a:_) -> if a /= 0 then 1 else lookup e
-                               (a:b:_) -> if b /= 0 then fastLook ds a else lookup e
+                               (a:b:_) -> fastLook ds a b
                                _       -> lookup e
   where e  = lastDigit' xs
         x' = if x < 20 then x else x `mod` 20 + 20
@@ -33,7 +43,7 @@ lastDigit' (x:xs) = case xs of (1:_)   -> lookup 1
         lookup a = case a of 0 -> 1
                              1 -> x'
                              j -> ds !! (fromIntegral $ j `mod` 4)
-        fastLook (a:b:c:_) e'
+        fastLook (a:b:c:_) e' e''
           | a == b    = a
-          | a == c    = if e' `mod` 2 == 0 then a else b
+          | a == c    = if e' `mod` 2 == 0 && e'' /= 0 then a else b
           | otherwise = lookup e
